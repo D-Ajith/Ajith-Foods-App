7029dad2afd08b0a11d200e7b148a3d619f66e60
D-Ajith <doddi.ajith2003@gmail.com>
2025-07-29 20:40:33 +0530

    Ajith-Foods

--- Ajith-Foods/src/baseURL.js
+++ Ajith-Foods/src/baseURL.js
@@ -1,6 +1,2 @@
-const baseURL =
-  window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
-    ? "http://localhost:5000/"
-    : "https://ajith-foods-app.onrender.com/";
-
+const baseURL = "https://ajith-foods-app.onrender.com/";
 export default baseURL;
